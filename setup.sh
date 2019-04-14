65d658f03668a5bca22e069d22f87a4198e14652
Amit <amitmoryossef@gmail.com>
2019-04-15 00:41:45 +0300

    fix(dependecies): bump opennmt version

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 # Install OpenNMT
 mkdir libs
 cd libs
-git clone --branch 0.7.0 https://github.com/OpenNMT/OpenNMT-py.git OpenNMT
+git clone --branch 0.7.1 https://github.com/OpenNMT/OpenNMT-py.git OpenNMT
 cd OpenNMT
 #python setup.py install
 pip install -r requirements.txt
